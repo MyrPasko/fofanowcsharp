8dbf516ffeea25f181bb47942c9b140fdd74622a
Myroslav Pasko <myroslavpasko@gmail.com>
2020-05-24 23:59:13 +0300

    first commit

---
+++ FofanovTestProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FofanovTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
