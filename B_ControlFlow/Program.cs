e1d2d3849816ef55ba75b9236db7a606f170b0bb
Myroslav Pasko <myroslavpasko@gmail.com>
2020-06-03 07:08:18 +0300

    On lesson 48. 40%

--- B_ControlFlow/Program.cs
+++ B_ControlFlow/Program.cs
@@ -6,14 +6,122 @@ namespace B_ControlFlow
     {
         static void Main(string[] args)
         {
-            HomeWork4();
+            HomeWork6();
+        }
+
+        static void HomeWork6()
+        {
+            var counter = 0;
+            int[] arr = new int[10];
+            int num = 0;
+            double result = 0;
+
+            while (counter < 10)
+            {
+                Console.WriteLine($"Gimme number: ");
+                num = int.Parse(Console.ReadLine());
+
+                if (num == 0)
+                {
+                    break;
+                }
+
+                arr[counter] = num;
+                counter++;
+            }
+
+            for (int i = 0; i < counter; i++)
+            {
+                result += arr[i];
+            }
+
+            Console.WriteLine($"The average is: {result / counter}");
+        }
+
+        static void HomeWork5()
+        {
+            int fibonacciCount = 0;
+
+            while (fibonacciCount <= 1)
+            {
+                Console.WriteLine($"How many Fibonacci numbers would you like to see?");
+                fibonacciCount = int.Parse(Console.ReadLine());
+            }
+
+            int[] fiboArr = new int[fibonacciCount];
+
+            for (int i = 0; i < fibonacciCount; i++)
+            {
+                if (i <= 1)
+                {
+                    fiboArr[i] = 1;
+                }
+                else
+                {
+                    fiboArr[i] = (fiboArr[i - 1] + fiboArr[i - 2]);
+                }
+            }
+
+            string showArr = $"The array is: ";
+
+            foreach (var num in fiboArr)
+            {
+                showArr += $"{num} ";
+            }
+
+            Console.WriteLine(showArr);
+        }
+
+        static void Debugging()
+        {
+            // Geron's formula for debugging
+
+            Console.WriteLine($"Gimme the first side: ");
+            double firstSide = GetDouble();
+
+            Console.WriteLine($"Gimme the second side: ");
+            double secondSide = GetDouble();
+
+            Console.WriteLine($"Gimme the third side: ");
+            double thirdSide = GetDouble();
+
+            var semiPerimeter = (firstSide + secondSide + thirdSide) / 2;
+            var triangleArea = Math.Sqrt(semiPerimeter *
+                                         (semiPerimeter - firstSide) *
+                                         (semiPerimeter - secondSide) *
+                                         (semiPerimeter - thirdSide));
+
+            Console.WriteLine($"Result is: {triangleArea}");
+        }
+
+        static double GetDouble()
+        {
+            return double.Parse(Console.ReadLine());
+        }
+
+        static void Cycles()
+        {
+            int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+
+            foreach (var num in nums)
+            {
+                if (num % 2 == 0 && num != 0)
+                {
+                    Console.WriteLine(num);
+                }
+            }
         }
 
         static void HomeWork4()
         {
             Console.WriteLine($"Gimme the first number: ");
             var firstNum = int.Parse(Console.ReadLine());
-            
+
             Console.WriteLine($"Gimme the second number: ");
             var secondNum = int.Parse(Console.ReadLine());
 
