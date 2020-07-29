78af503e9c80beca1af2bbf6a66095521de4d3d3
Myroslav Pasko <myroslavpasko@gmail.com>
2020-07-30 00:25:21 +0300

    On lesson 76

--- D_OOP/Calculator.cs
+++ D_OOP/Calculator.cs
@@ -23,5 +23,29 @@ namespace D_OOP
             double rads = corner * Math.PI / 180;
             return 0.5 * firstSide * secondSide * Math.Sin(rads);
         }
+
+        public int CheckOrderOfArgument(int arg1, int arg2, params int[] arg3)
+        {
+            int sum = 0;
+
+            foreach (var num in arg3)
+            {
+                sum += num;
+            }
+            
+            return arg2 - arg1 - sum;
+        }
+
+        public static bool TryDivide(int divisible, int divider, out int result)
+        {
+            result = 0;
+            if (divider == 0)
+            {
+                return false;
+            }
+
+            result = divisible / divider;
+            return true;
+        }
     }
 }
--- D_OOP/Program.cs
+++ D_OOP/Program.cs
@@ -12,13 +12,52 @@ namespace D_OOP
             //
             // Console.WriteLine(c.Health);
             
-            Calculator calc = new Calculator();
+            // Calculator calc = new Calculator();
+            //
+            // var square1 = calc.CalcTriangleSquare(20, 20, 30);
+            // var square2 = calc.CalcTriangleSquare(50, 50, 90);
+            //
+            // var rightOrder = calc.CheckOrderOfArgument(10, 20, 1, 1, 1);
+            // var namedRightOrder = calc.CheckOrderOfArgument(arg2: 15, arg1: 10);
+            //
+            // Console.WriteLine(square1);
+            // Console.WriteLine(square2);
+            //
+            // Console.WriteLine(rightOrder);
+            // Console.WriteLine(namedRightOrder);
+            //
+            // if (Calculator.TryDivide(10, 0, out int result))
+            // {
+            //     Console.WriteLine(result);
+            // }
+            // else
+            // {
+            //     Console.WriteLine(result);
+            // }
 
-            var square1 = calc.CalcTriangleSquare(20, 20, 30);
-            var square2 = calc.CalcTriangleSquare(50, 50, 90);
+            PointVal a;
+            a.X = 3;
+            a.Y = 5;
+
+            PointVal b = a;
+            b.X = 7;
+            b.Y = 10;
+
+            a.LogValues();
+            b.LogValues();
+
+            Console.WriteLine("After structs");
             
-            Console.WriteLine(square1);
-            Console.WriteLine(square2);
+            PointRef o = new PointRef();
+            o.X = 3;
+            o.Y = 5;
+
+            PointRef n = o;
+            n.X = 7;
+            n.Y = 10;
+
+            o.LogValues();
+            n.LogValues();
         }
     }
 }
